d4757a9c727fe25ae70d936a751d5b198b379873
Armand Emamdjomeh <armand.emamdjomeh@washpost.com>
2019-12-11 17:50:29 -0500

    I closed xquartz and it crashed R

--- scripts/maps_3d.r
+++ scripts/maps_3d.r
@@ -54,9 +54,65 @@ if (.Platform$OS.type == "windows") {
   freetype = TRUE
 }
 
+# reposition camera
+render_camera(theta=120, phi=20, zoom=0.3, fov=90)
+
+# clear any previous labels (in case we're re-running this code)
+render_label(clear_previous = TRUE)
+
 # add a label and render the snapshot
 render_label(hobart_mat, "River Derwent", textcolor = "white", linecolor = "white", freetype = freetype,
              x = 450, y = 260, z = 1400, textsize = 2.5, linewidth = 4, zscale = 10)
 render_snapshot(title_text = "render_label() demo, part 1", 
                 title_bar_alpha = 0.8,
                 title_bar_color = "white")
+
+# add another label
+render_label(hobart_mat, "Jordan River (not that one)", textcolor ="white", linecolor = "white", freetype = freetype,
+             x = 450, y = 140, z = 1400, textsize = 2.5, linewidth = 4, zscale = 10, dashed = TRUE)
+render_snapshot(title_text = "render_label() demo, part 2", 
+                title_bar_alpha = 0.8,
+                title_bar_color = "white")
+
+render_camera(zoom = 0.9, phi = 50, theta = -45,fov = 0)
+render_snapshot()
+
+
+# Now let's label mountains
+render_label(clear_previous = TRUE)
+render_label(hobart_mat, "Mount Faulkner", textcolor ="white", linecolor = "white", freetype = freetype,
+             x = 135, y = 130, z = 2500, textsize = 2, linewidth = 3, zscale = 10, clear_previous = TRUE)
+render_snapshot()
+
+render_label(hobart_mat, "Mount Dromedary", textcolor ="white", linecolor = "white", freetype = freetype, 
+             x = 320, y = 390, z = 1000, textsize = 2, linewidth = 3, zscale = 10)
+render_snapshot()
+
+# clear labels
+render_label(clear_previous = TRUE, freetype = freetype)
+render_snapshot()
+
+# close the render window
+rgl::rgl.close()
+
+
+# what you can do depth of field! 
+# Note this and the below lines do not work if you run them all at once (I got an error)
+# run them separately and it will work
+hobart_mat %>%
+  sphere_shade(sunangle = 60) %>%
+  add_water(detect_water(hobart_mat), color = "lightblue") %>%
+  add_shadow(ray_shade(hobart_mat, sunangle = 60, sunaltitude = 3, zscale = 33, lambert = FALSE), max_darken = 0.5) %>%
+  add_shadow(lamb_shade(hobart_mat, sunangle = 60, sunaltitude = 3, zscale = 33), max_darken = 0.7) %>%
+  add_shadow(ambientshadows, max_darken = 0.1) %>%
+  plot_3d(hobart_mat, zscale = 10,windowsize = c(1000,1000), 
+          background = "#edfffc", shadowcolor = "#273633")
+
+render_camera(theta = 120, phi = 20, zoom = 0.3, fov = 90)
+# 1 is the background, 0 is the foreground
+render_depth(focus = 0.8, preview_focus = TRUE)
+
+render_depth(focus=0.81, focallength = 200, title_bar_color = "black", vignette=TRUE, 
+             title_text = "The River Derwent, Tasmania", title_color = "white", title_size = 50)
+
+
